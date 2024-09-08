89cad51cdb2a40a41f3ecdd5bae5c42587317626
Deepak2210-droid <deepaknkl10@gmail.com>
2024-09-08 21:01:54 +0530

    Adds getSingleMovie method based on the ImdbId passed in uri param

--- src/main/java/dev/deepak/movies_ms/MoviesController.java
+++ src/main/java/dev/deepak/movies_ms/MoviesController.java
@@ -3,10 +3,12 @@ package dev.deepak.movies_ms;
 import dev.deepak.movies_ms.pojo.Movie;
 import dev.deepak.movies_ms.repo.MoviesRepository;
 import dev.deepak.movies_ms.service.MoviesService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,12 @@ public class MoviesController {
     public ResponseEntity<List<Movie>> getAllMovies(){
 
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getSingleMovie(@PathVariable String id){
+
+        return new ResponseEntity<Movie>(movieService.singleMovies(id),HttpStatus.OK);
     }
 
 }
--- src/main/java/dev/deepak/movies_ms/service/MoviesService.java
+++ src/main/java/dev/deepak/movies_ms/service/MoviesService.java
@@ -2,6 +2,7 @@ package dev.deepak.movies_ms.service;
 
 import dev.deepak.movies_ms.pojo.Movie;
 import dev.deepak.movies_ms.repo.MoviesRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +17,10 @@ public class MoviesService {
     public List<Movie> allMovies(){
 
         return movieRepository.findAll();
+    }
+
+    public Movie singleMovies(String id){
+
+        return movieRepository.findByImdbId(id).orElse(null);
     }
 }
